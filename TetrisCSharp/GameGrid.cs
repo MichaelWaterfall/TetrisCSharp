3e28d168dd67735451420ccf91b659ea1b6de3f2
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2024-08-22 11:25:30 +0100

    public variables added

--- TetrisCSharp/GameGrid.cs
+++ TetrisCSharp/GameGrid.cs
@@ -10,6 +10,13 @@ namespace TetrisCSharp
     {
         private readonly int[,] grid;
 
+        public int Rows { get; }
+        public int Columns { get; }
 
+        public int this[int r, int c]
+        {
+            get => grid[r, c];
+            set => grid[r, c] = value;
+        }
     }
 }
