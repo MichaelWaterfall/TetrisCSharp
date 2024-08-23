6b9f41c28f25c9e66d3bf1a3a5312fbefbdd69f7
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2024-08-23 13:46:42 +0100

    IBlock class added

---
+++ TetrisCSharp/IBlock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisCSharp
+{
+    public class IBlock : Block
+    {
+
+    }
+}
