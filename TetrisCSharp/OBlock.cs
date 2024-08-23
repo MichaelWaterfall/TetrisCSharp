3cb91d61c32d1cb4e87cccb34a6632d94fc30d62
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2024-08-23 14:15:40 +0100

    JBlock class added

--- TetrisCSharp/OBlock.cs
+++ TetrisCSharp/OBlock.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace TetrisCSharp
 {
-    internal class OBlock
+    public class OBlock : Block
     {
+        private readonly Position[][] tiles = new Position[][]
+        {
+            new Position[] { new(0,0), new(0,1), new(0,1), new(1,1)},
+        };
+
+        public override int Id => 4;
+
+        protected override Position StartOffset => new Position(0, 4);
+
+        protected override Position[][] Tiles => tiles;
     }
 }
